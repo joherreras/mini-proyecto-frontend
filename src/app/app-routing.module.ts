75e3190b93ea8f7917450d28be446b3b13a84fe7
Josefa Herrera <josefaigna.herreras@gmail.com>
2020-08-26 21:16:55 -0300

    cambios para todas las vistas y arreglando el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { PortalComponent } from "./components/portal/portal.component";
 import { CursosComponent } from "./components/cursos/cursos.component";
 import { HomeComponent } from "./components/home/home.component";
 import { LoginComponent } from './components/login/login.component';
+
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "home", component: HomeComponent },
